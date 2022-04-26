431c3bf6ef68e0762f291ff4d404ecdc50398a8e
Chimoha31 <mokochii1108@gmail.com>
2022-04-26 16:58:59 -0700

    Implemented first post

--- src/features/posts/AddPostForm.js
+++ src/features/posts/AddPostForm.js
@@ -1,14 +1,59 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
+import {useDispatch} from 'react-redux';
+import {nanoid} from '@reduxjs/toolkit'; //uuid()のようにランダムでidを生成してくれる
+import {postAdded} from './postSlice';
 
 const AddPostForm = () => {
+  const dispatch = useDispatch();
+
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
+
+  const onTitleChanged = (e) => {
+    setTitle(e.target.value);
+  } 
+  const onContentChanged = (e) => {
+    setContent(e.target.value);
+  } 
+
+  const onSavePostClicked = () => {
+    if(title && content) {
+      dispatch(
+        postAdded({
+          id: nanoid(),
+          title,
+          content,
+        })
+      )
+      setTitle("");
+      setContent("");
+    }
+  }
 
   return (
     <section>
       <h2>Add a New Post</h2>
+      <form>
+        <label htmlFor="postTitle">Post Title:</label>
+        <input
+          type="text"
+          id="postTitle"
+          name="postTitle"
+          value={title}
+          onChange={onTitleChanged}
+        />
+        <label htmlFor="postContent">Content:</label>
+        <textarea
+          type="text"
+          id="postContent"
+          name="postContent"
+          value={content}
+          onChange={onContentChanged}
+        ></textarea>
+        <button type="button" onClick={onSavePostClicked}>Save Post</button>
+      </form>
     </section>
-  )
-}
+  );
+};
 
-export default AddPostForm
+export default AddPostForm;
